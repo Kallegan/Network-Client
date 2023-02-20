a8334df81fe9bc5f45146b1dd444d353c4cc38bc
Kallegan <90606627+Kallegan@users.noreply.github.com>
2023-02-20 21:24:19 +0100

    Initial commit

---
+++ Network-Client/main.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <WinSock2.h> //used for open sockets on operative system.
+
+int main()
+{
+	//version 2.2,
+	WSADATA wsa_data; //data struct
+	WSAStartup(MAKEWORD(2, 2), &wsa_data); //initialize winsock lib.
+
+	printf("Enter port: ");
+	unsigned short port;
+	scanf_s("%hu", &port); //%hu = reads half unsigned (unsigned short).
+
+	//an unsiged int that acts as a handle to the socket.
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //internet commuicating using pct protocol. 
+
+	if (sock == INVALID_SOCKET)
+	{
+		printf("FAIELD TO CREATE SOCKET!\n");
+		return 1;
+	}
+	//local host.
+	unsigned char ip_bytes[] = { 192, 168, 50, 2 }; //treats as if its a int in memory by converting and de-referensing. 
+	unsigned int ip = *(unsigned int*)ip_bytes;
+
+	sockaddr_in conn_addr; //handles ipv4 socket adressing.
+	conn_addr.sin_family = AF_INET;
+	conn_addr.sin_addr.s_addr = ip;
+
+	//windows uses small endian while the internet uses big endian, so we have to convert.
+	conn_addr.sin_port = htons(port); //host to network short(port)
+
+	int connect_result = connect(sock, (sockaddr*)&conn_addr, sizeof(conn_addr));
+
+	if (connect_result)
+	{
+		printf("CONNECTION FAILED! Error: %d\n", WSAGetLastError());
+	}
+
+	//send data
+	static const char* my_messege = "CLIENT CONNECTED!";
+	send(sock, my_messege, strlen(my_messege), 0);
+
+
+	//recv data
+	char buffer[1024];
+	int recv_size = recv(sock, buffer, 1024, 0); //client waits for msg due to recv.
+	buffer[recv_size] = 0;
+
+	printf("Message received: (%s)\n", buffer);
+
+	return 0;
+}
